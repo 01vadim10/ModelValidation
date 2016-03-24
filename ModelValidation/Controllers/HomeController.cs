22fe1cca600c1cdf75009393aecde113846fbcdf
Vadim Zylevich <01vjob10@gmail.com>
2016-03-24 13:12:07 +0300

    1st commit

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Mvc;
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if (string.IsNullOrEmpty(appt.ClientName))
+            {
+                ModelState.AddModelError("ClientName", "Please enter your name");
+            }
+
+            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
+            {
+                ModelState.AddModelError("Date", "Please enter a date in the future");
+            }
+
+            if (!appt.TermsAccepted)
+            {
+                ModelState.AddModelError("TermsAccepeted", "You must accept the terms");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
