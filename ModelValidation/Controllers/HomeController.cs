0a0ca68244b482e0265065d11eafc3db16855963
vadimZ <01vadim10@gmail.com>
2016-03-28 10:50:08 +0300

    The final commit

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -14,21 +14,6 @@ namespace ModelValidation.Controllers
         [HttpPost]
         public ViewResult MakeBooking(Appointment appt)
         {
-            if (string.IsNullOrEmpty(appt.ClientName))
-            {
-                ModelState.AddModelError("ClientName", "Please enter your name");
-            }
-
-            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
-            {
-                ModelState.AddModelError("Date", "Please enter a date in the future");
-            }
-
-            if (!appt.TermsAccepted)
-            {
-                ModelState.AddModelError("TermsAccepeted", "You must accept the terms");
-            }
-
             if (ModelState.IsValid)
             {
                 return View("Completed", appt);
@@ -36,6 +21,25 @@ namespace ModelValidation.Controllers
             else
             {
                 return View();
+            }
+        }
+
+        public JsonResult ValidateDate(string Date)
+        {
+            DateTime parsedDate;
+            if (!DateTime.TryParse(Date, out parsedDate))
+            {
+                return Json("Please enter a valid date (mm/dd/yyyy)",
+                    JsonRequestBehavior.AllowGet);
+            }
+            else if (DateTime.Now > parsedDate)
+            {
+                return Json("Please enter a date in the future",
+                    JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
             }
         }
     }
